9456ea07dace9664e3d4651e93b7a00eff493005
seojungmin <neraize@gmail.com>
2022-02-25 15:41:51 +0900

    Random클래스를 활용한, 랜덤값 뽑아오기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,10 +1,23 @@
 package codes;
 
+import java.util.Random;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 
+		// 컴퓨터가 세자리 숫자 출제
+		// int 세칸짜리 배열로
 		
+		int[] cpuNumbers =  new int[3];
+		
+		for(int i=0; i<cpuNumbers.length; i++) {
+			
+			while(true) {
+				Random myRandom =new Random();
+				int randomNum = myRandom.nextInt(9)+1; // 1 ~10직전 (9)
+			}
+		}
 		
 	}
 }
